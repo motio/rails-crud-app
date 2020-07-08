c2aadcec68bf69d970daaffacaf60544675840e7
Keita Sakamoto <keita.sakamoto.work@gmail.com>
2020-07-08 12:38:36 +0900

    Update 20200630091335_create_questions.rb

--- db/migrate/20200630091335_create_questions.rb
+++ db/migrate/20200630091335_create_questions.rb
@@ -1,6 +1,8 @@
 class CreateQuestions < ActiveRecord::Migration[5.2]
   def change
+    # table の作成、 questions という名前で
     create_table :questions do |t|
+      # 保有するカラムは下記に定義
       t.string :name
       t.string :title
       t.text :content
